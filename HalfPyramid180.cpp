bf3794203cbbbaaf2eb38a81dedae86f43de898f
JayArcher9 <143174572+JayArcher9@users.noreply.github.com>
2024-06-11 23:55:19 +0530

    Create HalfPyramid180.cpp

---
+++ HalfPyramid180.cpp
@@ -0,0 +1,47 @@
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+
+//     int rows;
+//     cout<<"Enter the number of rows: ";
+//     cin>>rows;
+
+//     for(int i = rows ; i > 0; i--){
+//         for(int j = 1; j<=rows; j++){
+//             if(j<i){
+//                 cout<<" ";
+//             }
+//             else{
+//                 cout<<"*";
+//             }
+//         }
+//         cout<<endl;
+//     }
+//     return 0;
+// }
+
+
+
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int rows;
+    cout<<"Enter the number of rows: ";
+    cin>>rows;
+
+    for(int i = 0 ; i <= rows; i++){
+        for(int j = 1; j<=rows; j++){
+            if(j<rows-i){
+                cout<<" ";
+            }
+            else{
+                cout<<"*";
+            }
+        }
+        cout<<endl;
+    }
+    return 0;
+}
